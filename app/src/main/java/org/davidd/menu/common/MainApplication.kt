6ef6e81cf8c27bbdfe8ceebb3c78fca56c46fb90
David Debre <david.debre@softvision.ro>
2019-01-07 13:08:41 +0200

    dagger

--- app/src/main/java/org/davidd/menu/common/MainApplication.kt
+++ app/src/main/java/org/davidd/menu/common/MainApplication.kt
@@ -3,30 +3,20 @@ package org.davidd.menu.common
 import android.app.Activity
 import android.app.Application
 import android.content.Context
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import dagger.android.*
+import org.davidd.menu.data.DataService
+import org.davidd.menu.data.InMemoryDataService
+import org.davidd.menu.view.OrdersActivity
+import org.davidd.menu.viewmodel.OrdersViewModel
 import javax.inject.Inject
 
 class MainApplication : Application(), HasActivityInjector {
 
     @Inject
-    lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>
-
-    override fun activityInjector(): AndroidInjector<Activity> {
-        return dispatchingActivityInjector
-    }
-
-    init {
-        instance = this
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-
-//        DaggerMainApplicationComponent.create()
-//                .inject(this)
-    }
+    lateinit var activityInjector: DispatchingAndroidInjector<Activity>
 
     companion object {
         private var instance: MainApplication? = null
@@ -35,4 +25,42 @@ class MainApplication : Application(), HasActivityInjector {
             return instance!!.applicationContext
         }
     }
+
+    init {
+        instance = this
+    }
+
+    @Override
+    override fun onCreate() {
+        super.onCreate()
+
+        DaggerMainApplicationComponent.create().inject(this)
+    }
+
+    @Override
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return activityInjector
+    }
 }
+
+@Module
+abstract class MainApplicationModule {
+
+    @ContributesAndroidInjector // TODO ???
+    abstract fun contributeActivityInjector(): OrdersActivity
+}
+
+// responsible for injecting the MainApplication class
+@Component(modules = arrayOf(AndroidInjectionModule::class, MainApplicationModule::class, OrdersActivityModule::class))
+interface MainApplicationComponent : AndroidInjector<MainApplication>
+
+@Module
+class OrdersActivityModule(val dataService: DataService = InMemoryDataService) {
+
+    @Provides
+    fun provideOrdersViewModel(): OrdersViewModel {
+//        ordersViewModel = ViewModelProviders.of(this, OrdersViewModelFactory(InMemoryDataService)).get(OrdersViewModel::class.java)
+        return OrdersViewModel(dataService) // TODO this is bad
+    }
+}
+
--- app/src/main/java/org/davidd/menu/view/OrdersActivity.kt
+++ app/src/main/java/org/davidd/menu/view/OrdersActivity.kt
@@ -1,19 +1,18 @@
 package org.davidd.menu.view
 
 import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_orders.*
 import kotlinx.android.synthetic.main.content_orders.*
 import org.davidd.menu.R
-import org.davidd.menu.data.InMemoryDataService
 import org.davidd.menu.viewmodel.OrdersViewModel
-import org.davidd.menu.viewmodel.OrdersViewModelFactory
+import javax.inject.Inject
 
 class OrdersActivity : AppCompatActivity() {
 
@@ -21,12 +20,15 @@ class OrdersActivity : AppCompatActivity() {
         val TAG = OrdersActivity::class.java.simpleName
     }
 
-    private lateinit var ordersViewModel: OrdersViewModel
+    @Inject
+    lateinit var ordersViewModel: OrdersViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_orders)
         setSupportActionBar(toolbar)
+
+        AndroidInjection.inject(this)
 
         val orderIdEditText = findViewById<EditText>(R.id.order_id)
         val addOrderButton = findViewById<Button>(R.id.add_order)
@@ -35,7 +37,6 @@ class OrdersActivity : AppCompatActivity() {
             ordersViewModel.addNewOrder(orderIdEditText.text.toString())
         }
 
-        ordersViewModel = ViewModelProviders.of(this, OrdersViewModelFactory(InMemoryDataService)).get(OrdersViewModel::class.java)
         ordersViewModel.getOrdersLiveData().observe(this, Observer<String> { t ->
             Log.d(TAG, "onChanged")
             orderIdEditText.text = null
